f99db6c45bcfa35e7e6e97a968889a2a80416d1a
Greyson Christoforo <grey@christoforo.net>
2022-11-15 16:20:28 +0000

    fix installer

--- install.py
+++ install.py
@@ -3,10 +3,11 @@
 import json
 import shutil
 import os.path as osp
+from pathlib import Path as Plp
 
 mappings_file = "mappings.json"
 
 with open(mappings_file, 'rb') as fp:
     mappings = json.load(fp)
     for mapping in mappings:
-        shutil.copy2(mapping["name"], mapping["path"])
+        shutil.copy2(mapping["name"], Plp(mapping["path"]).expanduser())
